29df89a4b1c150cb3a9f5caf4f4ae62eab4d8524
robertBrem <brem_robert@hotmail.com>
2016-09-30 17:57:55 +0200

    initial commit

---
+++ src/main/java/ch/adesso/heros/JAXRSConfiguration.java
@@ -0,0 +1,10 @@
+package ch.adesso.heros;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+
+}
---
+++ src/main/java/ch/adesso/heros/boundary/HeroResource.java
@@ -0,0 +1,14 @@
+package ch.adesso.heros.boundary;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("heros")
+public class HeroResource {
+
+    @GET
+    public String getHeros() {
+        return "Black Panther, Spiderman, Ironman";
+    }
+
+}
