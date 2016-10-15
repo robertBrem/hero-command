8a5b3fb563cbdc2a06d32b34fa3ea04ce25d0166
robertBrem <brem_robert@hotmail.com>
2016-10-15 16:43:47 +0200

    use USER_NAME in imageName remove email

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 #!/usr/bin/jjs -fv
 
-var imageName = "robertbrem/heros-command:1.0." + $ENV.VERSION;
+var imageName = $ENV.USER_NAME + "/heros-command:1.0." + $ENV.VERSION;
 var build = "docker build -t " + imageName + " .";
 $EXEC(build);
 print($OUT);
 print($ERR);
-var dockerLogin = "docker login --username=" + $ENV.USER_NAME + " --password=" + $ENV.PASSWORD + " --email=" + $ENV.EMAIL;
+var dockerLogin = "docker login --username=" + $ENV.USER_NAME + " --password=" + $ENV.PASSWORD;
 $EXEC(dockerLogin);
 print($OUT);
 print($ERR);
