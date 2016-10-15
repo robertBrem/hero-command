b8c2b000d10ffab08d3f537d972267252aee51f3
robertBrem <brem_robert@hotmail.com>
2016-10-15 18:57:54 +0200

    print imageName

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 #!/usr/bin/jjs -fv
 
 var imageName = "docker.io/" + $ENV.USER_NAME + "/heros-command:" + $ENV.VERSION;
+print(imageName);
 var build = "docker build -t " + imageName + " .";
 $EXEC(build);
 print($OUT);
