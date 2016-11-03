ecdd0c8e66748abb462eae8c0e70873b3656e868
robertBrem <brem_robert@hotmail.com>
2016-11-03 23:11:32 +0100

    use own docker registry

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 #!/usr/bin/jjs -fv
 
-var imageName = "docker.io/" + $ENV.USER_NAME + "/heros-command:" + $ENV.VERSION;
+var imageName = "registry.disruptor.ninja:5000/" + $ENV.USER_NAME + "/heros-command:" + $ENV.VERSION;
 print(imageName);
 var build = "docker build -t " + imageName + " .";
 $EXEC(build);
