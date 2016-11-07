face48203dabf236126c4ac4e63982e280d0bf90
robertBrem <brem_robert@hotmail.com>
2016-11-07 17:07:18 +0100

    use own docker registry

--- build.js
+++ build.js
@@ -1,12 +1,12 @@
 #!/usr/bin/jjs -fv
 
-var imageName = "registry.disruptor.ninja:5000/" + $ENV.USER_NAME + "/heros-command:" + $ENV.VERSION;
+var imageName = "registry.disruptor.ninja:30500/" + $ENV.USER_NAME + "/heros-command:" + $ENV.VERSION;
 print(imageName);
 var build = "docker build -t " + imageName + " .";
 $EXEC(build);
 print($OUT);
 print($ERR);
-var dockerLogin = "docker login --username=rob --password=1234 registry.disruptor.ninja:5000";
+var dockerLogin = "docker login --username=rob --password=1234 registry.disruptor.ninja:30500";
 $EXEC(dockerLogin);
 print($OUT);
 print($ERR);
